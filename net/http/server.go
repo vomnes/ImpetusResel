849ce0a132325b9c26a390502e7dfdde74d159ec
Valentin Omnès <valentin.omnes@gmail.com>
2020-02-17 01:24:36 +0100

    feat: Add handle FdSet functions

--- net/http/server.go
+++ net/http/server.go
@@ -23,6 +23,30 @@ type server struct {
 	addrIPv4 *syscall.SockaddrInet4
 }
 
+// FdSet -> [32]int32
+// Max FD = 1024 = 32x32
+var fdSet syscall.FdSet
+
+// FDZero set to zero the fdSet
+func FDZero(p *syscall.FdSet) {
+	p.Bits = [32]int32{}
+}
+
+// FDSet actives a given bit of fdSet
+func FDSet(fd int, p *syscall.FdSet) {
+	p.Bits[fd/32] |= (1 << (uint(fd) % 32))
+}
+
+// FDClr actives a given bit of fdSet
+func FDClr(fd int, p *syscall.FdSet) {
+	p.Bits[fd/32] &^= (1 << (uint(fd) % 32))
+}
+
+// FDIsSet return true if the given fd is set
+func FDIsSet(fd int, p *syscall.FdSet) bool {
+	return p.Bits[fd/32]&(1<<(uint(fd)%32)) == 1
+}
+
 type data struct {
 	server server
 	router *Router
